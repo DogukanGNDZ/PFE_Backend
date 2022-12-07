6b1c5b77a31fcdc528f61fa151c6f1926d03fbbb
Golrak837 <maxime.ferriere@student.vinci.be>
2022-12-07 16:23:34 +0100

    resolve connexion neo4j problem

--- src/data/UserToDatabase.py
+++ src/data/UserToDatabase.py
@@ -5,17 +5,20 @@ from dotenv import load_dotenv
 from dataclasses import asdict
 
 load_dotenv()
-host=os.getenv("HOST")
-user=os.getenv("USER")
-password=os.getenv("PWD")
+host = os.getenv("HOST")
+user = os.getenv("USER")
+password = os.getenv("AUTH")
+
 
 # Connect to the database
 graph = GraphDatabase.driver(host, auth=(user, password))
 
+
 def create_user(user_dto: UserDTO):
     with graph.session() as session:
         # Create the new user in the Neo4j database
-        result = session.run('CREATE (u:User $user_properties) RETURN u', user_properties=asdict(user_dto))
+        result = session.run(
+            'CREATE (u:User $user_properties) RETURN u', user_properties=asdict(user_dto))
 
         user = result.single().data()['u']
         user.pop('password', None)
