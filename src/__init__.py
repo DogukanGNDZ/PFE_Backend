abea87f41f1d6ff30f15adb0320ee480b173395e
dogukan <dogukan.gunduz@student.vinci.be>
2022-12-09 19:02:48 +0100

    refactor code, login token + check token

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from src.routes.users import users_bp
 from src.routes.clubs import clubs_bp
 from src.routes.coachs import coachs_bp
+from src.routes.auth import auth_bp
 
 
 def create_app():
@@ -10,4 +11,5 @@ def create_app():
     app.register_blueprint(users_bp)
     app.register_blueprint(clubs_bp)
     app.register_blueprint(coachs_bp)
+    app.register_blueprint(auth_bp)
     return app
--- src/routes/users.py
+++ src/routes/users.py
@@ -41,32 +41,3 @@ def register():
     return create_user(user)
 
 
-def generate_jwt(user_id):
-    # Create the payload with the user's ID and an expiration date
-    payload = {
-        'user_id': user_id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
-    }
-
-    # Sign the JWT using a secret key
-    # do not forget to put this key in the env filee
-    jwt_token = jwt.encode(payload, 'MBJDT_Corporation')
-
-    return jwt_token
-
-
-@users_bp.route("/login", methods=["POST"])
-@cross_origin()
-def login():
-    password = request.json.get('password')
-    byte_pwd = password.encode('UTF-8')
-    email = request.json.get('email')
-    if check_user(byte_pwd, email):
-        get_user = fetch_user_email(email)
-        print(get_user[id])
-        return generate_jwt(get_user[id])
-
-    else:
-        response = make_response("Wrong password", 400)
-        return response
-
