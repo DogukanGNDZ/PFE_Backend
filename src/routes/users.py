6833c2aa48d0174089d8599a731b3c7aedcf256c
dogukan <dogukan.gunduz@student.vinci.be>
2022-12-12 16:45:26 +0100

    fix update

--- src/routes/users.py
+++ src/routes/users.py
@@ -76,11 +76,9 @@ def update_data_user():
     picture = request.json.get('picture')
     
     id=fetch_user_email(email)["id"]
-    print(fetch_user_email(email))
     if ast.literal_eval(claims.data.decode('utf-8'))["user_id"]!=id:
         return make_response('Not authorized', 401)
-    
-    print("avant create user dto")
+
     user = UserDTO(0, firstname, lastname, age, email, "", size,
                    weight, post, nYE, description, picture)
     return update_user(user)
